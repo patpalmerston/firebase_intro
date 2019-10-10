1718f09c4b1528f3ce8aba116063135eab56821a
Pat Palmerston <patpalmerston@gmail.com>
2019-10-10 14:57:49 -0700

    structure firebase app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import {database} from './firebase';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App--header">
-          <h2>Welcome to React and Firebase</h2>
-        </div>
-        <pre className="App--data">
-          One day, some data from Firebase will go here.
-        </pre>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: null
+		};
+	}
+	render() {
+		return (
+			<div className='App'>
+				<div className='App--header'>
+					<h2>Welcome to React and Firebase</h2>
+				</div>
+				<pre className='App--data'>
+					One day, some data from Firebase will go here.
+				</pre>
+			</div>
+		);
+	}
 }
 
 export default App;
