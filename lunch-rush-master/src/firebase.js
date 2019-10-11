67f89c60458046c35297aa5e4c833d15f34f07f0
Pat Palmerston <patpalmerston@gmail.com>
2019-10-10 20:11:00 -0700

    config

--- lunch-rush-master/src/firebase.js
+++ lunch-rush-master/src/firebase.js
@@ -1,11 +1,13 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+	apiKey: 'AIzaSyCE917gT1tzf7g-D7FDfnY9nrhvz_Hhzb4',
+	authDomain: 'fir-lunch.firebaseapp.com',
+	databaseURL: 'https://fir-lunch.firebaseio.com',
+	projectId: 'fir-lunch',
+	storageBucket: 'fir-lunch.appspot.com',
+	messagingSenderId: '452764998055',
+	appId: '1:452764998055:web:681ff586821c58cb9a7e82'
 };
 
 firebase.initializeApp(config);
